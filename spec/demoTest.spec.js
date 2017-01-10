4ee1ed7d68e170db3a197878797aa2ac56b65567
Chiamaka Nwosu <chiamaka.nwosu@andela.com>
2017-01-11 00:54:15 +0100

    adds jasmine test and config

---
+++ spec/demoTest.spec.js
@@ -0,0 +1,8 @@
+//jasmine test suite
+describe("Demo test", function() {
+	// it function forthe spec
+	it("should expect true to be true", function() {
+		//expect assertion function
+		expect(true).toBe(true);
+	});
+});
