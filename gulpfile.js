e7df7ab022ad6932731007acc60638955033881e
andela-oaboluwarin <oreoluwa.aboluwarin@andela.com>
2017-01-11 22:08:22 +0100

    chore<config>: update dependencies and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,69 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const mocha = require('gulp-mocha');
 const jshint = require('gulp-jshint');
+const mocha = require('gulp-mocha');
 const nodemon = require('gulp-nodemon');
-const bower = require('gulp-bower');
 const sass = require('gulp-sass');
+const bower = require('gulp-bower');
+const jade = require('gulp-jade');
 
 //Default task(s).
-gulp.task('default', ['jshint', 'server', 'watch', 'sass', 'test']);
+gulp.task('default', ['jshint', 'server', 'watch', 'sass']);
 
-// Project Configuration
-//const fs = require('fs');
-//const json = JSON.parse(fs.readFileSync('package.json'));
-gulp.task('nodemon', function() {
-    nodemon({
-        script: 'server.js',
-        ext: 'js',
-        env: { NODE_ENV: 'development' }
-    })
-});
-
-gulp.task('watch', function() {
-    gulp.watch(['public/css/common.scss', 'public/css/views/articles.scss'], ['sass']);
-    gulp.watch("app/views/**").on('change', browserSync.reload);
-    gulp.watch(['public/js/**', 'app/**/*.js'], ['jshint']).on('change', browserSync.reload);
-    gulp.watch("public/views/**").on('change', browserSync.reload);
-    gulp.watch('public/css/**', ['sass']).on('change', browserSync.reload);
-    gulp.watch('app/views/**', ['jade']).on('change', browserSync.reload);
-});
-
-
-gulp.task('jshint', function() {
+//jshint task
+gulp.task('jshint', () => {
     return gulp.src([
             'gulpfile.js',
             'app/**/*.js',
             'test/**/*.js',
             'public/js/**/*.js'
         ]).pipe(jshint())
-        .pipe(jshint.reporter('fail'));
+        .pipe(jshint.reporter('default'));
 });
 
+gulp.task('mochaTest', () => {
+    gulp.src('test/**/*.js', { read: false })
+        .pipe(mocha({ reporter: 'spec' }));
+});
 
+//Nodemon task
+gulp.task('nodemon', () => {
+    nodemon({
+        script: 'server.js',
+        ext: 'js'
+    })
+});
 
-gulp.task('server', ['nodemon'], function() {
+//Sass Task
+gulp.task('sass', () => {
+    return gulp.src('public/css/common.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/css/'));
+});
+
+//Jade Task
+gulp.task('jade', () => {
+    return gulp.src("app/views/**")
+        .pipe(jade())
+        .pipe(gulp.dest('public/views/'));
+});
+
+//Bower Task
+gulp.task('bower', () => {
+    bower()
+        .pipe(gulp.dest('./public/lib/'))
+});
+
+//Watch Task
+gulp.task('watch', () => {
+    gulp.watch('public/css/*.scss', ['sass']);
+    gulp.watch("app/views/**", ['jade']);
+    gulp.watch('app/**/*.js', ['jshint']);
+    gulp.watch("public/**/**").on('change', browserSync.reload);
+});
+
+//Server Task
+gulp.task('server', ['nodemon'], () => {
     browserSync.create({
         server: 'server.js',
         port: 3000,
@@ -50,34 +71,5 @@ gulp.task('server', ['nodemon'], function() {
     });
 });
 
-gulp.task('mochaTest', function() {
-    gulp.src('test/**/*.js', { read: false })
-        // gulp-mocha needs filepaths so you can't have any plugins before it
-        .pipe(mocha({ reporter: 'spec' }));
-});
-
-gulp.task('sass', function() {
-    return gulp.src('public/css/common.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('public/css/'));
-});
-
-gulp.task('bower', function() {
-    bower()
-        .pipe(gulp.dest('./public/lib/'))
-});
-
-
-
-
 //Test task.
-gulp.task('test', ['mochaTest'], function() {
-
-});
-
-
-
-//Bower task.
-gulp.task('install', ['bower'], function(done) {
-    done();
-});
+gulp.task('test', ['mochaTest']);
