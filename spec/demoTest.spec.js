50949efb1693cdac8d4c044924c4681187b9e1e1
Chiamaka Nwosu <chiamaka.nwosu@andela.com>
2017-01-11 23:19:49 +0100

    fix string quotation issues

--- spec/demoTest.spec.js
+++ spec/demoTest.spec.js
@@ -1,7 +1,7 @@
 //jasmine test suite
-describe("Demo test", function() {
+describe('Demo test', function() {
 	// it function forthe spec
-	it("should expect true to be true", function() {
+	it('should expect true to be true', function() {
 		//expect assertion function
 		expect(true).toBe(true);
 	});
