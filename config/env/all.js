c7b7fe5e10600eeaff71ee18c8e5a6edb029703f
andela-oaboluwarin <oreoluwa.aboluwarin@andela.com>
2017-01-16 19:51:43 +0100

    chore<edit>: update all.js file in env directory

--- config/env/all.js
+++ config/env/all.js
@@ -2,15 +2,8 @@ var path = require('path'),
     rootPath = path.normalize(__dirname + '/../..');
 var keys = rootPath + '/keys.txt';
 
-
 module.exports = {
-
     root: rootPath,
     port: process.env.PORT || 3000,
     db: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/test'
-};
-
-root: rootPath,
-    port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL
 };
