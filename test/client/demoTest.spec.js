88ea5d878ae67a3027f665d4b3b16a666523ac78
andela-oaboluwarin <oreoluwa.aboluwarin@andela.com>
2017-01-20 03:16:03 +0100

    remove unexpected token causing test to fail

--- test/client/demoTest.spec.js
+++ test/client/demoTest.spec.js
@@ -6,6 +6,6 @@ describe('Demo test', function() {
     // it function forthe spec
     it('should expect true to be true', function() {
         //expect assertion function
-        expect(true).to.be.(true);
+        expect(true).to.be.true;
     });
 });
