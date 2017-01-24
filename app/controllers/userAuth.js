76131746fc2bc7de8985c44f44b6c998bb4c61b6
Ike Grace <grace.ike@andela.comzenith007git config --global user.name andela-gike>
2017-01-24 16:58:33 +0100

    chore(styling): convert .jshint to .eslintrc.json

--- app/controllers/userAuth.js
+++ app/controllers/userAuth.js
@@ -1,12 +1,14 @@
-'use strict';
+
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+
 const User = mongoose.model('User');
 const moment = require('moment');
+
 const secret = process.env.SECRET_TOKEN_KEY;
 
-module.exports.login = function(req, res) {
-  let body = req.body;
+module.exports.login = (req, res) => {
+  const body = req.body;
 
   if (!body.email && !body.password) {
     return res.status(400).json({
@@ -15,13 +17,13 @@ module.exports.login = function(req, res) {
     });
   }
 
-  User.findOne({ 'email': body.email }, function(err, user) {
+  User.findOne({ 'email': body.email }, (err, user) => {
     if (err) {
       return res.status(500).send(err);
     }
     if (user && user.authenticate(body.password)) {
-      let expires = moment().add('days', 7).valueOf();
-      let token = jwt.sign({
+      const expires = moment().add('days', 7).valueOf();
+      const token = jwt.sign({
         iss: user._id,
         exp: expires,
       }, secret);
