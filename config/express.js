b51515546c08cd7e4fa4150afab351a61e8ad413
Otoloye Oyeniran <OT@Otoloyes-MacBook-Pro.local>
2017-01-25 15:52:44 +0100

    feature(converting-codebase-to-es6):refactored server.js and express.js

--- config/express.js
+++ config/express.js
@@ -1,19 +1,19 @@
 /**
  * Module dependencies.
  */
-var express = require('express'),
-  session = require('express-session'),
-  mongoStore = require('connect-mongo')(session),
-  flash = require('connect-flash'),
-  helpers = require('view-helpers'),
-  config = require('./config');
+const express = require('express');
+const session = require('express-session');
+const mongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+const helpers = require('view-helpers');
+const config = require('./config');
 
-module.exports = function(app, passport, mongoose) {
+module.exports = (app, passport, mongoose) => {
   app.set('showStackError', true);
 
   //Should be placed before express.static
   app.use(express.compress({
-    filter: function(req, res) {
+    filter: (req, res) => {
       return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
     },
     level: 9
@@ -33,60 +33,59 @@ module.exports = function(app, passport, mongoose) {
   app.set('view engine', 'jade');
 
   //Enable jsonp
-  app.enable('jsonp callback');
+  app.enable("jsonp callback");
 
-  app.configure(function() {
-    //cookieParser should be above session
-    app.use(express.cookieParser());
+  //cookieParser should be above session
+  app.use(express.cookieParser());
 
-    //bodyParser should be above methodOverride
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
+  //bodyParser should be above methodOverride
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
 
-    //express/mongo session storage
-    app.use(express.session({
-      secret: 'MEAN',
-      store: new mongoStore({
-        url: config.db,
-        collection: 'sessions',
-        mongooseConnection: mongoose.connection
-      })
-    }));
+  //express/mongo session storage
+  app.use(express.session({
+    secret: 'MEAN',
+    store: new mongoStore({
+      url: config.db,
+      collection: 'sessions',
+      mongooseConnection: mongoose.connection
+    })
+  }));
 
-    //connect flash for flash messages
-    app.use(flash());
+  //connect flash for flash messages
+  app.use(flash());
 
-    //dynamic helpers
-    app.use(helpers(config.app.name));
+  //dynamic helpers
+  app.use(helpers(config.app.name));
 
-    //use passport session
-    app.use(passport.initialize());
-    app.use(passport.session());
+  //use passport session
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-    //routes should be at the last
-    app.use(app.router);
+  //routes should be at the last
+  app.use(app.router);
 
-    //Assume "not found" in the error msgs is a 404. this is somewhat silly, but valid, you can do whatever you like, set properties, use instanceof etc.
-    app.use(function(err, req, res, next) {
-      //Treat as 404
-      if (~err.message.indexOf('not found')) return next();
+  //Assume "not found" in the error msgs is a 404. this is somewhat silly, but valid, you can do whatever you like, set properties, use instanceof etc.
+  app.use(function (err, req, res, next) {
+    //Treat as 404
+    if (!err.message.indexOf('not found')) {
+      return next();
+    };
 
-      //Log it
-      console.error(err.stack);
+    //Log it
+    console.error(err.stack);
 
-      //Error page
-      res.status(500).render('500', {
-        error: err.stack
-      });
+    //Error page
+    res.status(500).render('500', {
+      error: err.stack
     });
+  });
 
-    //Assume 404 since no middleware responded
-    app.use(function(req, res, next) {
-      res.status(404).render('404', {
-        url: req.originalUrl,
-        error: 'Not found'
-      });
+  //Assume 404 since no middleware responded
+  app.use(function (req, res, next) {
+    res.status(404).render('404', {
+      url: req.originalUrl,
+      error: 'Not found'
     });
-
   });
 };
