e99678ba42b85eb65418204d9fc2e72a42eb9a1d
Ike Grace <grace.ike@andela.comzenith007git config --global user.name andela-gike>
2017-01-29 20:08:42 +0100

    Refactoring the javascript styling guide in the signupAuth.js and routes.js

--- app/controllers/signupAuth.js
+++ app/controllers/signupAuth.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
-
 const User = mongoose.model('User');
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
-
 const secret = process.env.SECRET_TOKEN_KEY;
-
 
 module.exports.signup = (req, res) => {
   const body = req.body;
-
   if (!(body.name || body.email || body.password)) {
-    return res.json({ success: false,
+    return res.status(400).json({
+      success: false,
       message: 'Incomplete information. name, email and password are required.'
     });
   }
@@ -20,20 +17,24 @@ module.exports.signup = (req, res) => {
     email: req.body.email,
     password: req.body.password
   });
-
-  newUser.save((err, user) => {
+  newUser.save((err, User) => {
     if (err) {
-      res.status(400).json({
+      return res.status(400).json({
         success: false,
         message: 'cannot leave parameter empty'
       });
-    } else {
-      const expires = moment().add(7, 'days').valueOf();
-      const token = jwt.sign({ id: user.id, exp: expires }, 'secret');
-      res.json({
-        success: true,
-        message: 'Successfully created new user.', token, expires
-      });
     }
+    const expires = moment().add(7, 'days').valueOf();
+    const token = jwt.sign({
+      id: User.id,
+      exp: expires
+    }, 'secret');
+    res.json({
+      success: true,
+      message: 'Successfully created new user.',
+      token,
+      expires
+    });
+
   });
 };
