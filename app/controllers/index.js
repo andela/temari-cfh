a2082743bc0210ae2bb5172dc608698c66c4ff53
Ike Grace <grace.ike@andela.comzenith007git config --global user.name andela-gike>
2017-02-09 13:33:02 +0100

    feature(gametour): write function to redirect user to tour page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,14 +1,10 @@
-/**
- * Module dependencies.
- */
-var mongoose = require('mongoose'),
-  async = require('async'),
-  _ = require('underscore');
+// Module dependencies.
+const mongoose = require('mongoose');
+const async = require('async');
+const _ = require('underscore');
 
-/**
- * Redirect users to /#!/app (forcing Angular to reload the page)
- */
-exports.play = function(req, res) {
+// Redirect users to /#!/app (forcing Angular to reload the page)
+exports.play = (req, res) => {
   if (Object.keys(req.query)[0] === 'custom') {
     res.redirect('/#!/app?custom');
   } else {
@@ -16,7 +12,22 @@ exports.play = function(req, res) {
   }
 };
 
-exports.render = function(req, res) {
+/**
+ * Redirect users to the tour of the app
+ * @param{Stream} req - readable stream from the client
+ * @param{Stream} res - writable stream
+ * @return{undefined}
+ */
+exports.gameTour = (req, res) => {
+  if (Object.keys(req.query)[0] === 'custom') {
+    res.redirect('/#!/gametour?custom');
+  } else {
+    res.redirect('/#!/gametour');
+  }
+};
+
+
+exports.render = (req, res) => {
   res.render('index', {
     user: req.user ? JSON.stringify(req.user) : 'null'
   });
