82106f372cf8dbdcf86cf50da639426fb9f8ca9c
Chiamaka Nwosu <chiamaka.nwosu@andela.com>
2017-02-12 17:54:05 +0100

    feature (authentication): adds sign-in sign-up redesigned page

--- config/env/all.js
+++ config/env/all.js
@@ -4,14 +4,8 @@ const rootPath = path.normalize(__dirname + '/../..');
 // const keys = rootPath + '/keys.txt';
 
 module.exports = {
-<<<<<<< HEAD
-	root: rootPath,
-	port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL
-=======
   root: rootPath,
   port: process.env.PORT,
   db: process.env.MONGOHQ_URL,
   app: { name: 'Cards for Humanity' }
->>>>>>> develop
 };
