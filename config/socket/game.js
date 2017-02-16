239d681d78fbd00fb490b4908ea74628fdb5bcd6
Otoloye Oyeniran <OT@Otoloyes-MacBook-Pro.local>
2017-02-16 14:38:15 +0100

    feature(draw cards):hound issues

--- config/socket/game.js
+++ config/socket/game.js
@@ -274,7 +274,7 @@ Game.prototype.shuffleCards = function (cards) {
 
 Game.prototype.dealAnswers = function (maxAnswers) {
   maxAnswers = maxAnswers || 10;
-  const storeAnswers = function (err, data) {
+  var storeAnswers = function (err, data) {
     this.answers = data;
   };
   for (let i = 0; i < this.players.length; i += 1) {
