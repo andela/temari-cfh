7582679077f812adac18d396418a6bb610a165d7
Ike Grace <grace.ike@andela.comzenith007git config --global user.name andela-gike>
2017-02-27 17:26:28 +0100

    Feature:(chat) Adding chat information to the tour

--- public/js/controllers/onboard.js
+++ public/js/controllers/onboard.js
@@ -13,63 +13,69 @@ angular.module('mean.system')
     $scope.gameTour.setOption('showStepNumbers', false);
     $scope.gameTour.setOptions({
       steps: [{
-        intro: `This tour will serve as a shepherd, guiding you through
+          intro: `This tour will serve as a shepherd, guiding you through
         the process of playing Cards For Humanity. For navigation, use
         the arrow keys. You can use the skip option if you wish to continue
         directly into the game.`
-      },
-      {
-        element: '#question-container-outer',
-        intro: `You need a maximum of 11 and a minimum of 3 people to play.
+        },
+        {
+          element: '#question-container-outer',
+          intro: `You need a maximum of 11 and a minimum of 3 people to play.
         When there are 3 people, you can proceed.`
-      },
-      {
-        element: '#social-bar-container',
-        intro: `The avatar you chose is displayed here, you also find info
+        },
+        {
+          element: '#social-bar-container',
+          intro: `The avatar you chose is displayed here, you also find info
         about yourself and the game in progress, scores for each game round.
         When a player wins 5 rounds he/she becomes the winner.`
-      },
-      {
-        element: '#question-container-outer',
-        intro: `When you have the minimum required number of players for
+        },
+        {
+          element: '#question-container-outer',
+          intro: `When you have the minimum required number of players for
         a game, any player can start by clicking the START GAME button.
         The questions are displayed here in the game.`
-      },
-      {
-        element: '#cardsShow',
-        intro: `Different answers are shown here. Pick an answer most
+        },
+        {
+          element: '#cardsShow',
+          intro: `Different answers are shown here. Pick an answer most
         suitable for the question. Despicably too!`
-      },
-      {
-        element: '#timer-container',
-        intro: `The timer is displayed here. It counts down while players
+        },
+        {
+          element: '#timer-container',
+          intro: `The timer is displayed here. It counts down while players
         choose their answers, it also counts down as the CZAR decides
         the winner for that round.`
-      },
-      {
-        element: '#social-bar-container',
-        intro: 'Here, you get to know who the next CZAR is.'
-      },
-      {
-        element: '#join-new-game',
-        intro: `This allows you to join a new game because too many players
+        },
+        {
+          element: '#social-bar-container',
+          intro: 'Here, you get to know who the next CZAR is.'
+        },
+        {
+          element: '#join-new-game',
+          intro: `This allows you to join a new game because too many players
         left yours, or someone won the last game, and you wish to play another.`
-      },
-      {
-        element: '#game-end-container',
-        intro: `This allows you to go back to the lobby, join a new game
+        },
+        {
+          element: '#game-end-container',
+          intro: `This allows you to go back to the lobby, join a new game
         or make some children happy and survive by donating to their welfare.`
-      },
-      {
-        element: '#abandon-game-button',
-        intro: `If you don't wish to continue, click on this button, you
+        },
+        {
+          element: '.chatpanel',
+          intro: `Here as a player you can chat with other players
+        in an ongoing game`,
+          position: 'top'
+        },
+        {
+          element: '#abandon-game-button',
+          intro: `If you don't wish to continue, click on this button, you
         would be taken out of the game.`
-      },
-      {
-        element: '#tweet-container',
-        intro: `You can share your experience on twitter here, and invite
+        },
+        {
+          element: '#tweet-container',
+          intro: `You can share your experience on twitter here, and invite
         your friends to play with you.`
-      }
+        }
       ]
     });
     const isGameCustom = () => {
@@ -170,6 +176,16 @@ angular.module('mean.system')
             $scope.$apply(() => {
               $scope.showQuestion = false;
               $scope.gameEnd = true;
+              $scope.showChatBody = false;
+              $scope.expandChat = 'expand_less';
+            });
+            break;
+          }
+        case 'chatpanel':
+          {
+            $scope.$apply(() => {
+              $scope.showQuestion = false;
+              $scope.gameEnd = false;
               $scope.showChatBody = false;
               $scope.expandChat = 'expand_less';
             });
